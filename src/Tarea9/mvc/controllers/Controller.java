762fe9f9a987e816e7717657bd58b1a1c9aa814a
ArrestedRop <villagomezmaximo@gmail.com>
2026-08-28 08:34:15 -0600

    Ejercicio 9

---
+++ src/Tarea9/mvc/controllers/Controller.java
@@ -0,0 +1,34 @@
+package Tarea9.mvc.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Tarea9.mvc.models.*;
+import Tarea9.mvc.view.*;
+
+public class Controller {
+
+    private Model model;
+    private View view;
+    private ActionListener actionListener;
+
+    public Controller(Model model, View view){
+        this.model = model;
+        this.view = view;
+
+    }
+
+    public void contol(){
+        actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                linkBtnAndLabel();
+            }
+        };
+        view.getButton().addActionListener(actionListener);
+    }
+
+    private void linkBtnAndLabel(){
+        model.incX();
+        view.setText(Integer.toString(model.getX()));
+    }
+}
---
+++ src/Tarea9/mvc/models/Model.java
@@ -0,0 +1,22 @@
+package Tarea9.mvc.models;
+
+public class Model {
+
+    private int x;
+
+    public Model() {
+        x = 0;
+    }
+
+    public Model(int x) {
+        this.x = x;
+    }
+
+    public void incX() {
+        x++;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
